af75e7abca6181ae2f50bead672218033cd64375
Youssef Mohamed <79748625+YoussefMohamed7557@users.noreply.github.com>
2023-06-08 19:37:20 +0300

    finish sign up screen

--- ShopifyTeam4/modules/authentication/SignUpViewController.swift
+++ ShopifyTeam4/modules/authentication/SignUpViewController.swift
@@ -18,6 +18,12 @@ class SignUpViewController: UIViewController {
             whiteView.layer.cornerRadius = self.whiteView.bounds.width * 0.15
         }
     }
+    @IBOutlet weak var scrollableWhiteView: UIView!{
+        didSet{
+            scrollableWhiteView.layer.cornerRadius = self.scrollableWhiteView.bounds.width * 0.15
+        }
+    }
+    
     @IBOutlet weak var backButtonOutlet: UIButton!{
         didSet{
             backButtonOutlet.layer.cornerRadius = self.backButtonOutlet.bounds.width * 0.5
